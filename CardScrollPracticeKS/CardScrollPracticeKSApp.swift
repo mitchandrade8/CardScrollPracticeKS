5b6abc00f4e72df49b810d419e5c934af64cb335
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-05 21:33:38 -0700

    Initial Commit

---
+++ CardScrollPracticeKS/CardScrollPracticeKSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardScrollPracticeKSApp.swift
+//  CardScrollPracticeKS
+//
+//  Created by Mitch Andrade on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CardScrollPracticeKSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
